d3e4c047209398674916bafc3a142214c5588c05
ktr03 <ktrsouth@gmail.com>
2021-02-02 23:25:51 +0900

    improve util script

--- rsync.sh
+++ rsync.sh
@@ -3,5 +3,5 @@
 cd $(dirname $0)
 dir=$(basename `pwd`)
 
-fswatch -o . | xargs -I{} rsync -av --delete --exclude="target/" . pi@raspberrypi.local:work/${dir}
+fswatch -e "target/" -o . | xargs -I{} rsync -av --delete --exclude="target/" . pi@raspberrypi.local:work/${dir}
 
